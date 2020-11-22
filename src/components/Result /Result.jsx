36662032925eb8cdd80c7c63e8a00ba68aa1a815
Daniil_Sapielkin <vipdanik98@gmail.com>
2020-11-22 18:10:43 +0200

    add style

--- src/components/Result /Result.jsx
+++ src/components/Result /Result.jsx
@@ -1,43 +1,87 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import Table from "@material-ui/core/Table";
 
-import stl from "./Result.module.css";
+import { withStyles } from "@material-ui/core/styles";
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 16,
+  },
+}))(TableCell);
 
 export const Result = ({ keyInfo, scanKey, logout }) => {
   return (
-    <div className={stl.card}>
-      <div className={stl.cardInfo}>
-        <div>
-          <p>Login: </p>
-          <p>Password: </p>
-        </div>
-        <div className={stl.cardInfoTitle}>
+    <>
+      <Table style={{ width: "80%", margin: "auto" }}>
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Password</StyledTableCell>
+            <StyledTableCell>Email</StyledTableCell>
+            <StyledTableCell>QR code </StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableRow>
           {keyInfo.map((info) => (
-            <div key={info}>
-              <p>{info}</p>
-            </div>
+            <TableCell key={info}>{info}</TableCell>
           ))}
-        </div>
-      </div>
-      <Grid
-        container
-        direction="row"
-        justify="space-around"
-        alignItems="center"
+          <TableCell>
+            <a href={scanKey}>
+              <h3>{scanKey}</h3>
+            </a>
+          </TableCell>
+        </TableRow>
+      </Table>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={logout}
+        style={{ marginTop: "30px" }}
       >
-        <div className={stl.infoScan}>
-          Link on QR:
-          <a href={scanKey}>
-            <h3>{scanKey}</h3>
-          </a>
-        </div>
-        <div>
-          <Button variant="contained" color="primary" onClick={logout}>
-            Logout in
-          </Button>
-        </div>
-      </Grid>
-    </div>
+        Logout in
+      </Button>
+    </>
   );
+  // return (
+  //   <div className={stl.card}>
+  //     <div className={stl.cardInfo}>
+  //       <div>
+  //         <p>Login: </p>
+  //         <p>Password: </p>
+  //       </div>
+  //       <div className={stl.cardInfoTitle}>
+  //         {keyInfo.map((info) => (
+  //           <div key={info}>
+  //             <p>{info}</p>
+  //           </div>
+  //         ))}
+  //       </div>
+  //     </div>
+  //     <Grid
+  //       container
+  //       direction="row"
+  //       justify="space-around"
+  //       alignItems="center"
+  //     >
+  //       <div className={stl.infoScan}>
+  //         Link on QR:
+  //         <a href={scanKey}>
+  //           <h3>{scanKey}</h3>
+  //         </a>
+  //       </div>
+  //       <div>
+  //         <Button variant="contained" color="primary" onClick={logout}>
+  //           Logout in
+  //         </Button>
+  //       </div>
+  //     </Grid>
+  //   </div>
+  // );
 };
