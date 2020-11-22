f51355fe3c6d1909ebd93e9c8c1194016bb12d01
Daniil_Sapielkin <vipdanik98@gmail.com>
2020-11-22 18:11:10 +0200

    add style

--- src/components/Result /Result.jsx
+++ src/components/Result /Result.jsx
@@ -49,39 +49,4 @@ export const Result = ({ keyInfo, scanKey, logout }) => {
       </Button>
     </>
   );
-  // return (
-  //   <div className={stl.card}>
-  //     <div className={stl.cardInfo}>
-  //       <div>
-  //         <p>Login: </p>
-  //         <p>Password: </p>
-  //       </div>
-  //       <div className={stl.cardInfoTitle}>
-  //         {keyInfo.map((info) => (
-  //           <div key={info}>
-  //             <p>{info}</p>
-  //           </div>
-  //         ))}
-  //       </div>
-  //     </div>
-  //     <Grid
-  //       container
-  //       direction="row"
-  //       justify="space-around"
-  //       alignItems="center"
-  //     >
-  //       <div className={stl.infoScan}>
-  //         Link on QR:
-  //         <a href={scanKey}>
-  //           <h3>{scanKey}</h3>
-  //         </a>
-  //       </div>
-  //       <div>
-  //         <Button variant="contained" color="primary" onClick={logout}>
-  //           Logout in
-  //         </Button>
-  //       </div>
-  //     </Grid>
-  //   </div>
-  // );
 };
