ce01b5d2806b012cdf898df356ca25fa2fef41f7
Daniil_Sapielkin <vipdanik98@gmail.com>
2020-11-25 20:11:36 +0200

    update

--- src/index.js
+++ src/index.js
@@ -8,10 +8,11 @@ import { store } from "./redux/redux-store";
 import App from "./App";
 
 ReactDOM.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
+    ,
   </HashRouter>,
   document.getElementById("root")
 );
